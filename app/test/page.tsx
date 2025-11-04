28d1556d5b289217d6fab18d2ba28c0df1ac0c61
BLA <lamben2007@gmail.com>
2025-11-04 16:49:14 +0100

    Initial commit

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div style={{ padding: "2rem", textAlign: "center" }}>
+            <h1>Page Test</h1>
+            <p>Ceci est ma page test de mon portfolio !</p>
+        </div>
+    );
+}
